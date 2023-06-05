0d4fe57c08852f6a6d1748ed7dfcfdc9362b8a30
Aman Singh <amans@zignuts.com>
2023-06-05 10:52:23 +0530

    merge

--- packages/common/lib/src/theme/_colors_theme_extension.dart
+++ packages/common/lib/src/theme/_colors_theme_extension.dart
@@ -76,7 +76,6 @@ class ZigHotelsColors extends ThemeExtension<ZigHotelsColors> {
     required this.onSuccess,
     required this.successContainer,
     required this.onSuccessContainer,
-    required this.white,
     required this.darkBlue,
     required this.seaBlue,
     required this.oceanBlue,

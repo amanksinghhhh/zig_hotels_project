069ef6eb5f202f0ecacf48089b2d50c8d5f2d725
Aman Singh <amans@zignuts.com>
2023-06-13 13:27:07 +0530

    added room control screen for guest app

--- guests_app/lib/screens/room_control_screen/room_control_screen.dart
+++ guests_app/lib/screens/room_control_screen/room_control_screen.dart
@@ -1,10 +1,62 @@
+import 'package:common/common.dart';
+import 'package:dimensions_theme/dimensions_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:guests_app/screens/room_control_screen/components/staggered_layout_widget.dart';
+import 'package:network/core/shared_preferences/helper.dart';
+import 'package:network/core/shared_preferences/preferences.dart';
 
-class RoomControlScreen extends StatelessWidget {
+class RoomControlScreen extends StatefulWidget {
   const RoomControlScreen({Key? key}) : super(key: key);
 
   @override
+  State<RoomControlScreen> createState() => _RoomControlScreenState();
+}
+
+class _RoomControlScreenState extends State<RoomControlScreen> {
+  final SharedPreferenceHelper _sharedPreferenceHelper =
+      SharedPreferenceHelper(Preference());
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final theme = Theme.of(context);
+    final padding = EdgeInsetsOf(context);
+    return Scaffold(
+      backgroundColor: theme.zigHotelsColors.darkBlue,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(40.h),
+        child: const AppbarWidget(
+          title: "Room Controls",
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: padding.symmetric(
+            horizontal: Dimensions.medium,
+          ),
+          child: ListView(
+            shrinkWrap: true,
+            children: [
+              const Space(Dimensions.smaller),
+              StaggeredPage(
+                onService1Tap: () {},
+                onService2Tap: () {},
+                onService3Tap: () {},
+                onService4Tap: () {},
+                onService5Tap: () {},
+                onService6Tap: () {},
+                serviceName1: 'Main Lights',
+                serviceName2: 'Do Not Disturb',
+                serviceName3: 'Temperature Control',
+                serviceName4: 'Make Up Room',
+                serviceName5: 'Scenes',
+                serviceName6: 'Fan Control',
+              ),
+              const Space(Dimensions.smaller),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
